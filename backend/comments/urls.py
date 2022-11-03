8098ac1197c750516b2b64ca1a7e404dee015ff7
Michael Thompson <thompsonmichaelj2020@gmail.com>
2022-11-03 14:54:37 -0400

    Instructor review: created folders for components

--- backend/comments/urls.py
+++ backend/comments/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.get_all_comments),
-    path('new_comment/', views.user_comments),
-    path('<int:pk>/', views.user_comments),
+    path('', views.get_all_comments), #GET all
+    path('new_comment/', views.user_comments), #POST
+    path('<int:pk>/', views.user_comments), #GET for one user
+    
     
 ]
---
+++ frontend/Components/VideoPage/VideoPage.jsx
@@ -0,0 +1,16 @@
+// group0
+// SearchBar.jsx
+
+// group1
+// SearchPage.jsx
+// RelatedVideos.jsx
+
+// group2
+// VideoPage.jsx
+// VideoPlayer.jsx
+
+// group2a
+// CommentList.jsx
+// CommentForm.jsx
+// Comment.jsx
+// ReplyList.jsx
