281ab6202b84d3e5a08f9415f66cbc0a51b05a80
Michael Thompson <thompsonmichaelj2020@gmail.com>
2022-11-03 17:37:28 -0400

    Installed Bootstrap

--- frontend/src/pages/VideoPage/VideoPage.jsx
+++ frontend/src/pages/VideoPage/VideoPage.jsx
@@ -14,3 +14,6 @@
 // CommentForm.jsx
 // Comment.jsx
 // ReplyList.jsx
+
+
+//<iframe id="ytplayer" type="text/html" width="640" height="360" src = "https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http://example.com" frameborder = "0" ></iframe >
