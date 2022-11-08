d94695ef310c6a61cdcd00a6ddb5252a67d1ab2b
Michael Thompson <thompsonmichaelj2020@gmail.com>
2022-11-08 18:20:34 -0500

    Feat: Thumbnails appear with the related videos

--- frontend/src/components/RelatedVideos/RelatedVideos.jsx
+++ frontend/src/components/RelatedVideos/RelatedVideos.jsx
@@ -4,9 +4,9 @@ const RelatedVideos = (props) => {
     return (
         <>
             {props.related.map((video) => {
-                { console.log(`On RelatedVideos result, video inside map:`, video) }     
+                { console.log(`On RelatedVideos.jsx result, video inside map thumbnail:`, video.snippet.thumbnails.standard.url) }     
                 return (
-                    <p>{video.snippet.title}</p>
+                    <><p><img src={video.snippet.thumbnails.standard.url} className="thumbnails"  alt="Video thumbnail."  /></p><p>{video.snippet.title}</p><p>Channel: {video.snippet.channelTitle}</p></>
 
 
                 )
